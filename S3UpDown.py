b27e78f5068c68fe46b768099a9809459740645e
Pedro Renan Mussato <81251621+PedroMussato@users.noreply.github.com>
2023-05-19 11:37:25 -0300

    english version

--- S3UpDown.py
+++ S3UpDown.py
@@ -1,30 +1,51 @@
-# INSTALAR A BIBLIOTECA
-# pip install boto3
-
-# IMPORTAR A BIBLIOTECA
 import boto3
 
-# INICIAR UM CLIENT S3
-s3 = boto3.client('s3')
+def upload_object(bucket_name, local_file_path, s3_file_path):
+    s3 = boto3.client('s3')
+    try:
+        s3.upload_file(local_file_path, bucket_name, s3_file_path)
+        print(f"File '{local_file_path}' uploaded to '{s3_file_path}' in bucket '{bucket_name}'.")
+    except Exception as e:
+        print(f"Error uploading file to S3: {e}")
 
-#INSERIR OS DADOS DE CONEXÃO E CAMINHO
-bucket_name = 'seu_bucket'
-local_file_path = 'caminho_do_arquivo_local/arquivo.txt'
-s3_file_path = 'caminho_no_s3/arquivo.txt'
+def search_object(bucket_name, object_key):
+    s3 = boto3.client('s3')
+    try:
+        s3.head_object(Bucket=bucket_name, Key=object_key)
+        print(f"Object '{object_key}' found in bucket '{bucket_name}'.")
+    except:
+        print(f"Object '{object_key}' not found in bucket '{bucket_name}'.")
 
-# FAZENDO UPLOAD DO ARQUIVO
-s3.upload_file(local_file_path, bucket_name, s3_file_path)
+def download_object(bucket_name, object_key, local_file_path):
+    s3 = boto3.client('s3')
+    try:
+        s3.download_file(bucket_name, object_key, local_file_path)
+        print(f"File '{object_key}' from bucket '{bucket_name}' downloaded to '{local_file_path}'.")
+    except Exception as e:
+        print(f"Error downloading file from S3: {e}")
 
-# FAZENDO DOWNLOAD DO ARQUIVO
-response = s3.get_object(Bucket=bucket_name, Key=s3_file_path)
-content = response['Body'].read()
+def delete_object(bucket_name, object_key):
+    s3 = boto3.client('s3')
+    try:
+        s3.delete_object(Bucket=bucket_name, Key=object_key)
+        print(f"Object '{object_key}' deleted from bucket '{bucket_name}'.")
+    except Exception as e:
+        print(f"Error deleting object from S3: {e}")
 
-# Caminho onde você deseja salvar o arquivo no disco local
-local_file_path = 'caminho_no_seu_disco_local/arquivo.txt'
+# Example usage:
+bucket_name = 'your_bucket'
+local_file_path = 'local_file_path/file.txt'
+s3_file_path = 's3_path/file.txt'
+object_key = 'object_path_and_name'
 
-# GRAVANDO ARQUIVO NO DISCO
-with open(local_file_path, 'wb') as file:
-    file.write(content)
+# Upload the file
+upload_object(bucket_name, local_file_path, s3_file_path)
 
-# REMOVENDO O ARQUIVO
-s3.delete_object(Bucket=bucket_name, Key=s3_file_path)
+# Search for the object
+search_object(bucket_name, object_key)
+
+# Download the object
+download_object(bucket_name, object_key, local_file_path)
+
+# Delete the object
+delete_object(bucket_name, object_key)
